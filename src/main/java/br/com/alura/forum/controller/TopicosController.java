3cf36b32ae3a0882486374a7dd72c829e03384a2
YuriAugusto <yuri.komesu@uni9.edu.br>
2021-08-05 15:59:00 -0300

    Parte 04 Aula 02 Cadastrando tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,21 +3,30 @@ package br.com.alura.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController//essa anotação indica que por padrão todos os métodos da classe terão um @ResponseBody, indica ao Spring que o retorno do método deve ser devolvido como resposta (exibe no browser)
+@RequestMapping("/topicos")//está classe responde as requisições com o endereço /topicos
 public class TopicosController {
 	
 	@Autowired//injeção de dependência
 	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")//ao digitar o endereço o método é chamado
-	public List<TopicoDto> lista(String nomeCurso) {
+	@Autowired//injeção de dependência
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso) {//TopicoDto Output de dados
 		if (nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
 			return TopicoDto.converter(topicos);
@@ -26,5 +35,10 @@ public class TopicosController {
 			return TopicoDto.converter(topicos);
 		}
 	}
-
+	
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {//TopicoForm Input de dados
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+	}
 }
